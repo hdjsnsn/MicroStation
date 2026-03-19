8374d20b40ac925ace0bc5f1ff2e6f128863557b
晓颜桑 <1271686016@qq.com>
2026-03-19 23:01:33 +0800

    整体初始化修改

--- src/main/java/org/example/aicodemother/controller/HelloController.java
+++ src/main/java/org/example/aicodemother/controller/HelloController.java
@@ -10,8 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping
-    public BaseResponse<String> hello() {
+    @GetMapping public BaseResponse<String> hello() {
         return ResultUtils.success("hello");
     }
 }
