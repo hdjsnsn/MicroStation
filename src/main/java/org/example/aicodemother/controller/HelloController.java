c212759281170e3228fc8fb3475ff1d948884a96
晓颜桑 <1271686016@qq.com>
2026-03-19 12:55:38 +0800

    后端初始化

---
+++ src/main/java/org/example/aicodemother/controller/HelloController.java
@@ -0,0 +1,17 @@
+package org.example.aicodemother.controller;
+
+import org.example.aicodemother.common.BaseResponse;
+import org.example.aicodemother.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public BaseResponse<String> hello() {
+        return ResultUtils.success("hello");
+    }
+}
